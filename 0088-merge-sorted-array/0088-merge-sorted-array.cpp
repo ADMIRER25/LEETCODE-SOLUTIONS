c379eb6d32dc112bb56626b7b29c11584e1947e7
Subha Nayak <106899847+ADMIRER25@users.noreply.github.com>
2022-12-07 20:39:00 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.51%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,13 +1,37 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    void merge(vector<int>&a, int m, vector<int>&b, int n) {
-        //int temp = m;
-        a.resize(m);
-        for(int i = 0; i < n; i++)
+    void merge(vector<int>&a, int m, vector<int>&b, int n) 
+    { int i,j;
+      if(m == 0)
+      {
+        int idx = 0;
+        for(i = m; i < m + n; i++)
         {
-            a.push_back(b[i]);
+          a[i] = b[idx];
+          idx++;
         }
-        sort(a.begin(),a.end());
+      }
+      else if(n == 0) return;
+      
+     // cout << a.size() << endl;
+      for(i = 0; i < m; i++)
+      {
+        if(a[i] > b[0]) swap(a[i],b[0]);
+        
+        int first = b[0];
+        
+        for(j = 1; j < n && first > b[j];j++)
+        {
+          b[j-1] = b[j];
+        }
+        b[j-1] = first;
+      }
+      int idx = 0;
+      for(i = m; i < m + n; i++)
+      {
+        a[i] = b[idx];
+        idx++;
+      }
     }
 };
